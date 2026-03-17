0634981297860aba46c4d8425f52350a820ae389
dallinnd <156145316+dallinnd@users.noreply.github.com>
2026-03-17 16:18:55 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'anthropac-cache-v1';
-const urlsToCache = [
+const CACHE_NAME = 'anthropac-v1';
+const assetsToCache = [
   './',
   './index.html',
   './style.css',
@@ -10,19 +10,16 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assetsToCache);
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Return cached version or fetch from network
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
